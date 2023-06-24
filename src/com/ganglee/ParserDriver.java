f4dac44c5d2d2fc8645e6da7da8fd612e482c5f3
ganglee <ganglee@live.com>
2023-06-24 22:50:12 +0800

    update sql

--- src/com/ganglee/ParserDriver.java
+++ src/com/ganglee/ParserDriver.java
@@ -5,7 +5,7 @@ import org.antlr.v4.runtime.CommonTokenStream;
 
 public class ParserDriver {
     public static void main(String[] args) {
-        String query = "SELECT name FROM student where age > 18";
+        String query = "SELECT name FROM student WHERE age > 18";
         SqlBaseLexer lexer = new SqlBaseLexer(new ANTLRInputStream(query.toUpperCase()));
         SqlBaseParser parser = new SqlBaseParser(new CommonTokenStream(lexer));
         MyVisitor visitor = new MyVisitor();
